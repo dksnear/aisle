48d576835540ff2d4929ba1448407ece0419d642
dksnear <dksnear@gmail.com>
2016-02-25 21:18:47 +0800

    add sync method

--- lib/core/File.php
+++ lib/core/File.php
@@ -102,11 +102,27 @@ class File implements \JsonSerializable {
         return false;
 
     }
-
+	
+	public static function Copy($src,$dest){
+		
+		if(!file_exists($src))
+			return false;
+		
+		$dir = dirname($dest);
+		
+		if(!is_dir($dir)){
+			
+			if(!mkdir($dir,0777,true))
+				return false;
+		}
+		
+		return copy($src,$dest);
+	}
+	
     // 遍历当前文件下的所有文件
     public static function Each($fileName,$depth=-1,$fn) {
 
-        return new self($fileName,$depth,$fn);
+        return new self(self::FixName($fileName),$depth,$fn);
 
     }
 
@@ -115,7 +131,7 @@ class File implements \JsonSerializable {
 
         $files = array();
 
-        new self($fileName,$depth,function($file) use($fn,& $files) {
+        new self(self::FixName($fileName),$depth,function($file) use($fn,& $files) {
 
             $files []= is_callable($fn) ? $fn($file) : $file;
 
@@ -123,7 +139,131 @@ class File implements \JsonSerializable {
 
         return $files;
     }
-
+	
+	// 同步2个目录中的差异文件
+	// @src 源目录
+	// @dest 目标目录
+	// @filters 文件名排除正则列表
+	// @flags 1:非递归同步  2:双向同步 4:同步目标文件夹中不存在的文件 8:以修改时间较早的文件为源 16:关闭md5校验
+	// #return false | array('count'=>'被修改的文件数目','files'=>'被修改的文件集合')
+	
+	public static function Sync2($src,$dest,$filters=array(),$flags=0){
+		
+		$src = self::FixName($src);
+		$dest = self::FixName($dest);
+		
+		if(!is_dir($src))
+			return false;
+		
+		if(!is_dir($dest)){
+			
+			if(!mkdir($dest,0777,true))
+				return false;
+		}
+		
+		$out = array('count'=>0,'files'=>array());
+		
+		$filters = is_array($filters) ? $filters : array($filters);
+		
+		self::Each($src,(1 & $flags) == 1 ? 1 : -1,function($srcFile) use($src,$dest,$filters,$flags,& $out){
+			
+			if($srcFile->type == self::$FILETYPE['DIRECTORY'])
+				return;
+			
+			$srcFile = $srcFile->fullName;
+			$destFile = $dest.str_replace($src,'',$srcFile);
+	
+			if(!empty($filters)){
+				
+				$matches = false;
+				
+				foreach($filters as $filter){
+							
+					if(preg_match($filter,$srcFile)){
+						
+						$matches = true;
+						break;
+					}
+				}
+				
+				if($matches) return false;
+				
+			}
+			
+			if(!file_exists($destFile)){
+				
+				if((4 & $flags) == 4){
+					
+					$out['files'][$destFile] = self::Copy($srcFile,$destFile);
+					$out['count']++;
+				}
+				
+				return;
+			}
+			
+			if((16 & $flags) != 16 && md5_file($srcFile) == md5_file($destFile))
+				return;
+			
+			if((8 & $flags) == 8){
+				
+				$tempf = $srcFile;
+				$srcFile = $destFile;
+				$destFile = $tempf;
+			}
+			
+			$sut = filemtime($srcFile);
+			$dut = filemtime($destFile);
+			
+			if($sut == $dut) return;
+			
+			$out['files'][$sut > $dut ? $destFile : $srcFile] = $sut > $dut ? self::Copy($srcFile,$destFile): self::Copy($destFile,$srcFile);		
+			$out['count']++;
+						
+		});
+		
+		if((2 & $flags) == 2){
+			
+			$rout = self::Sync2($dest,$src,$filters,$flags^2);
+			$out['count'] = $out['count'] + $rout['count'];
+			$out['files'] = array_merge($out['files'],$rout['files']);
+		}
+		
+		return $out;
+		
+		
+	}
+	
+	// 同步多个目录中的差异文件
+	public static function Sync($dirs,$filters=array(),$flags=0){
+		
+		if(!is_array($dirs) || count($dirs) < 2)
+			return false;
+		
+		$out = array('count'=>0,'files'=>array());
+		
+		for($i=0;$i<count($dirs)-1;$i++){
+			
+			$cur_out = self::Sync2($dirs[$i],$dirs[$i+1],$filters,$flags);
+			if(empty($cur_out))
+				continue;
+			$out['count'] = $out['count'] + $cur_out['count'];
+			$out['files'] = array_merge($out['files'],$cur_out['files']);
+		}
+		
+		return $out;
+		
+	}
+	
+	public static function FixName($path){
+		
+		if(is_string($path))
+			return preg_replace('/^\\\\|^\/|\\\\$|\/$/','',preg_replace('/\\\\{2,}|\/{2,}/',DIRECTORY_SEPARATOR,preg_replace('/\\\\|\//',DIRECTORY_SEPARATOR,$path)));
+		if(is_array($path))
+			return self::FixName(implode(DIRECTORY_SEPARATOR,$path));
+		
+		return $path;
+	}
+	
     // @file_name 目标文件完整路径
     // @depth(int) 读取深度 如果为-1则读取所有层级
     // @fn 读取进行时委托 $fn($currentFile)
@@ -151,9 +291,13 @@ class File implements \JsonSerializable {
             $this->type = self::$FILETYPE['DIRECTORY'];
             $this->children = array();
         }
-
-        is_callable($fn) && $fn($this);
-
+		
+		if(is_callable($fn)){
+						
+			if($fn($this) === false)
+				return;
+		}
+		
         $this->readDir($this,$depth,$fn,$level);
 
     }
@@ -211,13 +355,14 @@ class File implements \JsonSerializable {
         {
             if ($hFile=='.'||$hFile=='..')
                 continue;
-            $file->children[$hFile] = new self($file->fullName.'/'.$hFile,$depth-1,$fn,$level,$file,$file->fileId);
+            $file->children[$hFile] = new self(self::FixName(array($file->fullName,$hFile)),$depth-1,$fn,$level,$file,$file->fileId);
 
         }
 
         closedir($hDir);
 
     }
+
 
 	// implements JsonSerializable
     public function jsonSerialize() {
